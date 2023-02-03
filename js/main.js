bd2e60c9e84e7caeeabcdbf78ab445d3e0fac504
soyeon kim <kimsoyeon1112@gmail.com>
2023-02-03 22:08:22 +0900

    [feat] 이름 수정/삭제 버튼 추카

--- js/main.js
+++ js/main.js
@@ -11,7 +11,34 @@ const nameText = document.querySelector("#nameText");
 const mainFocus = document.querySelector(".mainFocus");
 const todayFocus = document.querySelector("#todayFocus");
 const userTrue = document.querySelector("#userTrue");
+// 이름 수정/삭제
+const nameBtnMenu = document.querySelector(".nameBtnMenu");
+const nameModiBtn = document.querySelector("#nameModiBtn");
+const nameDelBtn = document.querySelector("#nameDelBtn");
 
+//메뉴show hidden
+function menuShow() {
+  console.log("포커스?");
+  nameBtnMenu.classList.add("focus");
+}
+function menuHidden() {
+  nameBtnMenu.classList.remove("focus");
+}
+//이름 위에 올리면 메뉴 show
+function handleMouseMove(event) {
+  let offsetX = event.offsetX;
+  let offsetY = event.offsetY;
+  console.log(offsetX, offsetY);
+
+  if (280 <= offsetX && offsetX < 600 && 0 <= offsetY && offsetY < 40) {
+    menuShow();
+  } else {
+    menuHidden();
+  }
+}
+nameText.addEventListener("mousemove", handleMouseMove);
+
+// 최초 이름 입력----
 //이름입력후 엔터키
 function submitName(event) {
   event.preventDefault();
@@ -29,16 +56,11 @@ function isName() {
 
   if (userNameKey) {
     inputNameForm.classList.add("none");
-    // clockDiv.classList.add("active");
-    // mainFocus.classList.add("active");
     userTrue.classList.add("active");
     nameText.innerText = userNameKey;
   } else {
     inputNameForm.classList.remove("none");
-    // userTrue.classList.remove("active");
-    // clockDiv.classList.remove("active");
     mainFocus.classList.remove("active");
   }
 }
 isName();
-// localStorage.setItem("name", "soyeon");
